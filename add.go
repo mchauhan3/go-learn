0d48c546ab7ec4fecff2d175cdbce8bd5b8688d4
mchauhan3 <mhchauhan3@gmail.com>
2024-06-26 11:37:19 -0700

    add link

--- add.go
+++ add.go
@@ -1,7 +1,8 @@
 // Package add provides utilities for addition operations
 package add
 
-// Add takes two integers and sums them up
+// Add takes two integers and sums them up. More information on the addition operation can be found [here].
+// [here]: https://www.mathsisfun.com/numbers/addition.html
 func Add(x, y int) int {
 	return x + y
 }
