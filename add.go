84761faa67cdc5e18df7b46528e2e7038a48a363
mchauhan3 <mhchauhan3@gmail.com>
2024-06-26 11:47:57 -0700

    adding generic add

--- add.go
+++ add.go
@@ -1,8 +1,16 @@
 // Package add provides utilities for addition operations
 package add
 
-// Add takes two integers and sums them up. More information on the addition operation can be found [here].
+import (
+	"golang.org/x/exp/constraints"
+)
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
+// Add takes two Number and sums them up. More information on the addition operation can be found [here].
 // [here]: https://www.mathsisfun.com/numbers/addition.html
-func Add(x, y int) int {
+func Add[T Number](x, y T) T {
 	return x + y
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/mchauhan3/go-learn
+module github.com/mchauhan3/go-learn/v2
 
 go 1.22.4
+
+require golang.org/x/exp v0.0.0-20240613232115-7f521ea00fb8
